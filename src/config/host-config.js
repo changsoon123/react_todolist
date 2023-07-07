30647b3206780101ce8aeefe38760d63c4f6d5e7
changsoon123 <sool0458@naver.com>
2023-07-07 15:37:55 +0900

    2023-07-07 / aws hostname complete

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -6,10 +6,10 @@ const clientHostName = window.location.hostname;
 let backEndHostName; // 백엔드 서버 호스트 이름
 
 if(clientHostName === 'localhost'){ //개발 중
-  // backEndHostName = 'http://localhost:8181';
+  backEndHostName = 'http://localhost:8181';
+  
+} else if(clientHostName === 'my-todoapp-2477.s3-website.ap-northeast-2.amazonaws.com'){ //배포해서 서비스 중
   backEndHostName = 'http://13.124.226.120';
-} else if(clientHostName === 'spring.com'){ //배포해서 서비스 중
-  backEndHostName = 'https://api.spring.com'
 }
 
 export const API_BASE_URL = backEndHostName;
